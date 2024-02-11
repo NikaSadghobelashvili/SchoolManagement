d8ae971ea5457f07f2f8ee467bb23753b34e2b91
NikaSadghobelashvili <113467699+NikaSadghobelashvili@users.noreply.github.com>
2024-02-11 19:46:30 +0400

    Add files via upload

---
+++ Services/ClassroomService.cs
@@ -0,0 +1,69 @@
+﻿using DTO;
+using Interfaces;
+using Repository;
+
+namespace Services
+{
+    public class ClassroomService : IClasroomService
+    {
+        private readonly IClassroomRepository _classroomRepository;
+        private readonly SchoolManagementDbContext _context;
+        public ClassroomService(IClassroomRepository classroomRepository, SchoolManagementDbContext context)
+        {
+            _classroomRepository = classroomRepository;
+            _context = context;
+        }
+
+        public void CreateClass(Classroom cl)
+        {
+            try
+            {
+                _context.Database.BeginTransaction();
+                _classroomRepository.Save();
+                _context.Database.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool DeleteClass(int id)
+        {
+            try
+            {
+                _context.Database.BeginTransaction();
+                var clasroom = _classroomRepository.GetById(id);
+                if (clasroom != null)
+                {
+                    _classroomRepository.Add(clasroom);
+                    _classroomRepository.Save();
+                    _context.Database.CommitTransaction();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public IEnumerable<Classroom> GetAllClasses()
+        {
+            return _classroomRepository.GetAll();
+        }
+
+        public void save()
+        {
+            _classroomRepository.Save();
+        }
+
+        public int UpdateClass(Classroom cl)
+        {
+          return  _classroomRepository.Update(cl);
+        }
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,74 @@
+﻿using DTO;
+using Interfaces;
+using Repository;
+
+namespace Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly SchoolManagementDbContext _context;
+        public StudentService(IStudentRepository studentRepository, SchoolManagementDbContext context)
+        {
+            _studentRepository = studentRepository;
+            _context= context;
+        }
+        public void CreateStudnet(Student s)
+        {
+            try
+            {
+                _context.Database.BeginTransaction();
+                _studentRepository.Add(s);
+                _studentRepository.Save();
+                _context.Database.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool DeleteStudent(int id)
+        {
+            try
+            {
+                _context.Database.BeginTransaction();
+                var student = _studentRepository.GetById(id);
+                if(student!=null)
+                {
+                    _studentRepository.Delete(id);
+                    _studentRepository.Save();
+                    _context.Database.CommitTransaction();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _studentRepository.GetAll();
+        }
+
+        public Student? GetStudentById(int id)
+        {
+            return _studentRepository.GetById(id);
+        }
+
+        public void save()
+        {
+            _studentRepository.Save();
+        }
+
+        public int UpdateStudent(Student s)
+        {
+          return _studentRepository.Update(s);
+        }
+    }
+}
